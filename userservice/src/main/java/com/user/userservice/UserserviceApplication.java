a021711da2c4c0c490dd38a127cbe4ee6655a9fb
Jorge Villalobos <villalobos.esparza.jorge@gmail.com>
2023-08-10 14:13:25 -0600

    eureka

--- userservice/src/main/java/com/user/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/user/userservice/UserserviceApplication.java
@@ -2,10 +2,12 @@ package com.user.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableEurekaClient
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
